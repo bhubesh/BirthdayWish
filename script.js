89df629e5b80b42087126c38f0bb56222b0beb7a
Bhubesh <Krboobesh@gmail.com>
2020-01-16 08:29:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var a=prompt("Enter YOUR NAME SISTER");
+  	if(a== "Janani")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
